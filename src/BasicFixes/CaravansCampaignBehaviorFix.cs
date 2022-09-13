8be0db94a194cb3dcbc683d64ddb07e76dc6db7f
Space-Pot8to <83307992+Space-Pot8to@users.noreply.github.com>
2022-09-13 19:44:31 -0400

    added settings file to turn off or on different fixes, added behavior to correct caravans being unable to find a town to trade with

---
+++ src/BasicFixes/CaravansCampaignBehaviorFix.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace BasicFixes
+{
+    /// <summary>
+    /// CaravansCampaignBehavior._previouslyChangedCaravanTargetsDueToEnemyOnWay is a dictionary 
+    /// that keeps a list of settlements that can't be traveled to for each caravan. The entry 
+    /// for a given caravan is only cleared when the caraven enters a settlement. What can 
+    /// sometimes happen, however, is that this list can grow to include all the settlements a 
+    /// carvan can travel to, meaning the caravan becomes stuck and won't move. This behavior 
+    /// fixes this problem by clearing the list when it reaches this state.
+    /// </summary>
+    public class CaravansCampaignBehaviorFix : CampaignBehaviorBase
+    {
+        private CaravansCampaignBehavior _original;
+
+        public override void RegisterEvents()
+        {
+            CampaignEvents.AiHourlyTickEvent.AddNonSerializedListener(this, new Action<MobileParty, PartyThinkParams>(this.AIHourlyTick));
+        }
+
+        public override void SyncData(IDataStore dataStore)
+        {
+            
+        }
+
+        private void AIHourlyTick(MobileParty mobileParty, PartyThinkParams partyThinkParams)
+        {
+            if (mobileParty.IsCaravan)
+            {
+                if (_original == null)
+                    _original = Campaign.Current.GetCampaignBehavior<CaravansCampaignBehavior>();
+
+                // get field through access tools since it's private
+                FieldInfo field = _original.GetType().GetField("_previouslyChangedCaravanTargetsDueToEnemyOnWay", BindingFlags.NonPublic | BindingFlags.Instance);
+                Dictionary<MobileParty, List<Settlement>> badDict = field.GetValue(_original) as Dictionary<MobileParty, List<Settlement>>;
+
+                // condition is from CaravansCampaignBehavior.FindNextDestinationForCaravan
+                List<Town> possibleTowns = Town.AllTowns
+                    .Where(x => x.Owner.Settlement != mobileParty.CurrentSettlement &&
+                                !x.IsUnderSiege && !x.MapFaction.IsAtWarWith(mobileParty.MapFaction) &&
+                                (!x.Settlement.Parties.Contains(MobileParty.MainParty) || !MobileParty.MainParty.MapFaction.IsAtWarWith(mobileParty.MapFaction))).ToList();
+
+                // clear the list when there are no more possible settlement for the caravan to travel to
+                if (badDict != null && badDict.ContainsKey(mobileParty) && badDict[mobileParty].Count == possibleTowns.Count()) 
+                {
+                    badDict[mobileParty].Clear();
+                    field.SetValue(_original, badDict);
+                }
+            }
+        }
+    }
+}
--- src/BasicFixes/SubModule.cs
+++ src/BasicFixes/SubModule.cs
@@ -1,25 +1,65 @@
 ﻿using System.Linq;
+using System.Xml;
+
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.CampaignBehaviors.AiBehaviors;
 using TaleWorlds.Core;
 using TaleWorlds.MountAndBlade;
 
+using Helpers;
+using TaleWorlds.ModuleManager;
+
 namespace BasicFixes
 {
     public class SubModule : MBSubModuleBase
     {
+        private bool enableDiscardFood = true;
+        private bool enableRecruitFix = true;
+        private bool enableCaravanFix = true;
+
         protected override void InitializeGameStarter(Game game, IGameStarter starter)
         {
             CampaignGameStarter campaignGameStarter = starter as CampaignGameStarter;
             if(campaignGameStarter != null)
             {
-                campaignGameStarter.AddBehavior(new DiscardExcessiveItemsBehavior());
+                XmlDocument settingsDoc = MiscHelper.LoadXmlFile(ModuleHelper.GetModuleFullPath("BasicFixes") + "ModuleData/settings.xml");
+                if(settingsDoc != null)
+                {
+                    XmlElement documentElement = settingsDoc.DocumentElement;
+                    XmlNodeList xmlNodeList = documentElement.SelectNodes("/Settings/*");
+                    foreach(XmlNode node in xmlNodeList)
+                    {
+                        if(node.Name == "DiscardExcessFood")
+                        {
+                            enableDiscardFood = bool.Parse(node.InnerText);
+                        }
+                        else if (node.Name == "AIRecruitFix")
+                        {
+                            enableRecruitFix = bool.Parse(node.InnerText);
+                        }
+                        else if (node.Name == "CaravanFix")
+                        {
+                            enableCaravanFix = bool.Parse(node.InnerText);
+                        }
+                    }
+                }
 
-                // remove bad AIVisitSettlementBehavior
-                AiVisitSettlementBehavior badBehavior = campaignGameStarter.CampaignBehaviors.FirstOrDefault(x => x is AiVisitSettlementBehavior) as AiVisitSettlementBehavior;
-                if(badBehavior != null)
-                    campaignGameStarter.RemoveBehavior(badBehavior);
-                campaignGameStarter.AddBehavior(new AiVisitSettlementBehaviorFixed());
+                if(enableDiscardFood)
+                    campaignGameStarter.AddBehavior(new DiscardExcessiveItemsBehavior());
+
+                if (enableRecruitFix)
+                {
+                    // remove bad AIVisitSettlementBehavior
+                    AiVisitSettlementBehavior badBehavior = campaignGameStarter.CampaignBehaviors.FirstOrDefault(x => x is AiVisitSettlementBehavior) as AiVisitSettlementBehavior;
+                    if (badBehavior != null)
+                        campaignGameStarter.RemoveBehavior(badBehavior);
+                    campaignGameStarter.AddBehavior(new AiVisitSettlementBehaviorFixed());
+                }
+
+                if (enableCaravanFix)
+                {
+                    campaignGameStarter.AddBehavior(new CaravansCampaignBehaviorFix());
+                }
             }
         }
     }
